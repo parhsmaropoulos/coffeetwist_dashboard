ec1cb4e845e11f75afd7d19b6a2f067dff7ab0c9
paris maropoulos <pmaropoulos@skroutzdelivery.gr>
2021-09-25 15:02:35 +0300

    Initial homepage design

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -30,15 +30,6 @@ class HomePage extends Component {
             <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
               Sign in to your account
             </h2>
-            <p class="mt-2 text-center text-sm text-gray-600">
-              Or
-              <a
-                href="#"
-                class="font-medium text-indigo-600 hover:text-indigo-500"
-              >
-                start your 14-day free trial
-              </a>
-            </p>
           </div>
           <form class="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" value="true" />
@@ -89,15 +80,6 @@ class HomePage extends Component {
                 >
                   Remember me
                 </label>
-              </div>
-
-              <div class="text-sm">
-                <a
-                  href="#"
-                  class="font-medium text-indigo-600 hover:text-indigo-500"
-                >
-                  Forgot your password?
-                </a>
               </div>
             </div>
 
