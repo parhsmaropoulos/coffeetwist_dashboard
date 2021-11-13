7354f12de757e41ab47dfab45b22ecfdbd2b2664
paris maropoulos <pmaropoulos@skroutzdelivery.gr>
2021-11-13 12:45:32 +0200

    Env config for aws

--- src/utils/util.js
+++ src/utils/util.js
@@ -3,10 +3,9 @@ export const options = {
   ShopSubTitle: "Ελληνικό, Αθήνα",
 };
 
-export const live_url = "";
-export const local_url = "http://localhost:8080/";
-
 export const current_url =
-  process.env.ENV === "production" ? "" : "http://localhost:8080/";
+  process.env.ENV === "production"
+    ? "https://ec2-54-170-72-108.eu-west-1.compute.amazonaws.com"
+    : "http://localhost:8080/";
 export const run_on =
   process.env.ENV === "production" ? "" : "http://localhost:3000";
