83b411922781319a88ab5c76af5e5506348e6005
paris maropoulos <pmaropoulos@skroutzdelivery.gr>
2021-12-04 13:23:35 +0200

    Fix prod url

--- src/utils/util.js
+++ src/utils/util.js
@@ -4,7 +4,7 @@ export const options = {
 };
 
 export const current_url =
-  "http://ct-lb-1094286185.eu-west-1.elb.amazonaws.com:8080";
+  "https://ct-lb-1094286185.eu-west-1.elb.amazonaws.com";
 
 export const run_on =
   process.env.ENV === "production" ? "" : "http://localhost:3000";
