fa4f0adb4450aa1b89f3c16c3ca3686b18f2ebbe
paris maropoulos <pmaropoulos@skroutzdelivery.gr>
2021-12-04 13:53:36 +0200

    Revert change

--- src/utils/util.js
+++ src/utils/util.js
@@ -3,7 +3,8 @@ export const options = {
   ShopSubTitle: "Ελληνικό, Αθήνα",
 };
 
-export const current_url = "https://www.coffeetwist.gr/";
+export const current_url =
+  "https://ct-lb-1094286185.eu-west-1.elb.amazonaws.com/";
 
 export const run_on =
   process.env.ENV === "production" ? "" : "http://localhost:3000";
