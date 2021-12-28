e45966c58c51611de7bd86447bfcb93313375fe9
paris maropoulos <pmaropoulos@skroutzdelivery.gr>
2021-12-28 21:43:27 +0200

    Remove connect socket method

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -14,37 +14,37 @@ socket.onopen = () => {
 
 socket.onclose = (event) => {
   console.log("Socket Closed Connection: ", event);
-  connect();
+  // connect();
 };
 
 socket.onerror = (error) => {
   console.log("Socket Error: ", error);
 };
 
-let connect = () => {
-  const socket = new WebSocket(
-    `${
-      process.env.REACT_APP_ENV === "dev"
-        ? "ws://localhost:8080"
-        : "wss://api.ct-dashboard.gr"
-    }/ws/admin`
-  );
+// let connect = () => {
+//   const socket = new WebSocket(
+//     `${
+//       process.env.REACT_APP_ENV === "dev"
+//         ? "ws://localhost:8080"
+//         : "wss://api.ct-dashboard.gr"
+//     }/ws/admin`
+//   );
 
-  console.log("Attempting Connection...");
+//   console.log("Attempting Connection...");
 
-  socket.onopen = () => {
-    console.log("Successfully Connected");
-  };
+//   socket.onopen = () => {
+//     console.log("Successfully Connected");
+//   };
 
-  socket.onclose = (event) => {
-    console.log("Socket Closed Connection: ", event);
-    connect();
-  };
+//   socket.onclose = (event) => {
+//     console.log("Socket Closed Connection: ", event);
+//     connect();
+//   };
 
-  socket.onerror = (error) => {
-    console.log("Socket Error: ", error);
-  };
-};
+//   socket.onerror = (error) => {
+//     console.log("Socket Error: ", error);
+//   };
+// };
 
 let sendMsg = (msg) => {
   //   console.log("sending msg: ", msg);
