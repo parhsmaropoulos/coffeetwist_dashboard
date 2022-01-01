dbc27fc4089e80c152a48fa1f00f226a4e37e68b
paris maropoulos <pmaropoulos@skroutzdelivery.gr>
2022-01-01 13:07:00 +0200

    Fix preview of images when creating objects

--- src/components/Forms/CreateItemForm.js
+++ src/components/Forms/CreateItemForm.js
@@ -193,6 +193,16 @@ const ItemForm = ({
     );
     isUpdate ? onUpdate(e, body) : onCreate(e, body);
   };
+
+  let previewUrl = "";
+  function onFileChange(e) {
+    if (e.target.files[0]) {
+      product.image = e.target.files[0];
+      previewUrl = URL.createObjectURL(e.target.files[0]);
+      document.getElementById("img-preview").src = previewUrl;
+      document.getElementById("image-preview-div").style.visibility = "visible";
+    }
+  }
   return (
     <form className="w-full max-w-lg items-center" onSubmit={onsubmit}>
       {" "}
@@ -389,42 +399,27 @@ const ItemForm = ({
         </div>
       </div>
       <div>
-        <label className="block text-sm font-medium text-gray-700">
+        <label className="block text-sm font-bold text-gray-700">
           Φωτογραφία
         </label>
-        <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-          <div className="space-y-1 text-center">
-            <svg
-              className="mx-auto h-12 w-12 text-gray-400"
-              stroke="currentColor"
-              fill="none"
-              viewBox="0 0 48 48"
-              aria-hidden="true"
-            >
-              <path
-                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            <div className="flex text-sm text-gray-600">
-              <label
-                htmlFor="file-upload"
-                className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-              >
-                <span>Εισάγετε αρχείο</span>
-                <input
-                  id="file-upload"
-                  name="file-upload"
-                  type="file"
-                  className="sr-only"
-                />
-              </label>
-            </div>
-            <p className="text-xs text-gray-500">PNG, JPG, GIF μέχρι 10MB</p>
-          </div>
+        <div className="flex text-sm text-gray-600">
+          <label
+            htmlFor="file-upload"
+            className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+          >
+            <span>Εισάγετε αρχείο</span>
+            <input
+              id="file-upload"
+              name="file-upload"
+              type="file"
+              className="sr-only"
+              onChange={onFileChange}
+            />
+          </label>
         </div>
+      </div>
+      <div id="image-preview-div" style={{ visibility: "hidden" }}>
+        <img id="img-preview" alt="img-preview" src={previewUrl} />
       </div>
       <SaveButton onSubmit={(e) => onsubmit(e)} />
     </form>
@@ -448,10 +443,14 @@ const CategoryForm = ({ isUpdate, item, onCreate, onUpdate }) => {
     body.append("description", category.description);
     isUpdate ? onUpdate(e, body) : onCreate(e, body);
   };
+  let previewUrl = "";
   function onFileChange(e) {
-    category.image = e.target.files[0];
-    document.getElementById("img-preview").src = category.image.name;
-    // console.log(document.getElementById("img-preview").src);
+    if (e.target.files[0]) {
+      category.image = e.target.files[0];
+      previewUrl = URL.createObjectURL(e.target.files[0]);
+      document.getElementById("img-preview").src = previewUrl;
+      document.getElementById("image-preview-div").style.visibility = "visible";
+    }
   }
   return (
     <form className="w-full max-w-lg items-center">
@@ -490,39 +489,27 @@ const CategoryForm = ({ isUpdate, item, onCreate, onUpdate }) => {
         </div>
       </div>
       <div>
-        <label className="block text-sm font-medium text-gray-700">
+        <label className="block text-sm font-bold text-gray-700">
           Φωτογραφία
         </label>
-        <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-          <div className="space-y-1 text-center">
-            <svg
-              className="mx-auto h-12 w-12 text-gray-400"
-              stroke="currentColor"
-              fill="none"
-              viewBox="0 0 48 48"
-              aria-hidden="true"
-            >
-              <img id="img-preview" alt="img-preview" src={category.image} />
-            </svg>
-
-            <div className="flex text-sm text-gray-600">
-              <label
-                htmlFor="file-upload"
-                className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-              >
-                <span>Εισάγετε αρχείο</span>
-                <input
-                  id="file-upload"
-                  name="file-upload"
-                  type="file"
-                  className="sr-only"
-                  onChange={onFileChange}
-                />
-              </label>
-            </div>
-            <p className="text-xs text-gray-500">PNG, JPG, GIF μέχρι 10MB</p>
-          </div>
+        <div className="flex text-sm text-gray-600">
+          <label
+            htmlFor="file-upload"
+            className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+          >
+            <span>Εισάγετε αρχείο</span>
+            <input
+              id="file-upload"
+              name="file-upload"
+              type="file"
+              className="sr-only"
+              onChange={onFileChange}
+            />
+          </label>
         </div>
+      </div>
+      <div id="image-preview-div" style={{ visibility: "hidden" }}>
+        <img id="img-preview" alt="img-preview" src={previewUrl} />
       </div>
       <SaveButton onSubmit={(e) => onsubmit(e)} />
     </form>
